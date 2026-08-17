6c18bdcc29997a5f569a5583407f96a311057795
shadratalmn7-sudo <shadrat.almn7@gmail.com>
2026-08-17 22:01:59 +0300

    Copy Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyA1EWlVkTThSIVwV7iOPIUwnWvB2tIg-yQ",
+  authDomain: "shazarat-platform.firebaseapp.com",
+  projectId: "shazarat-platform",
+  storageBucket: "shazarat-platform.firebasestorage.app",
+  messagingSenderId: "163989935844",
+  appId: "1:163989935844:web:c7b1a185da53076e6dbef1",
+  measurementId: "G-XEXLP7S36Z"
+});
