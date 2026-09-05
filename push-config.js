6f8659be908b8b099a8a9270552152bcc8c98694
shadratalmn7-sudo <shadrat.almn7@gmail.com>
2026-09-05 23:43:42 +0300

    Connect Shadrat website to OneSignal app

--- push-config.js
+++ push-config.js
@@ -1,6 +1,6 @@
 export const pushConfig = Object.freeze({
   provider: 'onesignal',
-  appId: '',
+  appId: 'fece206d-55fe-4ff4-a207-62421a870f16',
   serviceWorkerPath: '/OneSignalSDKWorker.js',
   manifestPath: '/manifest.webmanifest'
 });
